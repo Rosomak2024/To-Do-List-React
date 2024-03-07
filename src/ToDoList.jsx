77ec95ee2304f36007719df8331751296fc3a9e3
Rosomak2024 <mbnet@o2.pl>
2024-03-07 19:15:29 +0100

    new version with time

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -2,61 +2,75 @@ import { useState } from "react";
 import { ToDoListItem } from "./ToDoListItem";
 
 export const ToDoList = () => {
-  const [tasks, setTasks] = useState([]);
-  const [inputValue, setInputValue] = useState("");
-  const [inputEditValue, setInputEditValue] = useState("");
+  const [tasks, setTasks] = useState([
+    { name: "zrób pompki", time: 10 },
+    { name: "zrób przysiady", time: 6 },
+  ]);
+  const [task, setTask] = useState({ name: "", time: 0 });
 
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
-  };
-  const handleInputEditChange = (e) => {
-    setInputEditValue(e.target.value);
+  const actualNameChange = (value) => {
+    setTask({ ...task, name: value });
   };
 
   const handleAddTask = () => {
-    setTasks((prevState) => [...prevState, inputValue]);
-    setInputValue("");
+    setTasks((prevState) => [...prevState, task]);
   };
 
   const handleRemoveTask = (index) => {
     setTasks((prevTasks) => {
       const newTasks = [...prevTasks];
-      newTasks.splice(index, 1); //at  possition index remove 1 item.
+      newTasks.splice(index, 1);
       return newTasks;
     });
   };
 
-  const handleEditTask = (index) => {
+  const handleEditTask = (index, taskName, time) => {
     setTasks((prevTasks) => {
       const newTasks = [...prevTasks];
-      newTasks.splice(index, 1, inputEditValue);
-      // setInputEditValue("");
+      newTasks.splice(index, 1, { name: taskName, time: time });
+
       return newTasks;
     });
   };
+
+  const result = tasks.reduce((total, task) => {
+    const taskTime = parseFloat(task.time);
+    return total + taskTime;
+  }, 0);
 
   return (
     <>
       <input
-        id="input"
-        placeholder="add task..."
-        value={inputValue}
-        onChange={handleInputChange}
+        id="inputName"
+        placeholder="add task name..."
+        value={task.name}
+        onChange={(e) => actualNameChange(e.target.value)}
+      ></input>
+      <input
+        type="number"
+        id="inputTime"
+        placeholder="add task time..."
+        value={task.time}
+        onChange={(e) => setTask({ ...task, time: e.target.value })}
       ></input>
       <button id="add_btn" onClick={handleAddTask}>
         Add Task
       </button>
+      <h2>
+        Czas potrzebny do wykonania {tasks.length} zadań to: {result} min;
+        <br></br> Time needed to finish {tasks.length} tasks is: {result} min;
+      </h2>
       <div id="container">
         <ul>
           {tasks.map((task, index) => {
             return (
               <ToDoListItem
                 key={index}
-                taskName={task}
-                taskIndex={index}
+                name={task.name}
+                index={index}
                 handleRemoveTask={handleRemoveTask}
                 handleEditTask={handleEditTask}
-                handleInputEditChange={handleInputEditChange}
+                time={task.time}
               />
             );
           })}
--- src/ToDoListItem.jsx
+++ src/ToDoListItem.jsx
@@ -1,28 +1,43 @@
-import React from "react";
-// import { ToDoList } from "./ToDoList";
+import React, { useState } from "react";
 
 export const ToDoListItem = (props) => {
+  const [actualTaskName, setActualTaskName] = useState(props.name);
+  const [time, setTime] = useState(props.time);
+
   return (
     <>
-      <li key={props.taskIndex}>{props.taskName}</li>
-      <input
-        id={props.taskIndex}
-        onChange={props.handleInputEditChange}
-        value={props.inputEditValue}
-        placeholder="edit task here"
-      ></input>
-      <button
-        id="remove_btn"
-        onClick={() => props.handleRemoveTask(props.taskIndex)}
-      >
-        Remove
-      </button>
-      <button
-        id="edit_btn"
-        onClick={() => props.handleEditTask(props.taskIndex)}
-      >
-        Edit
-      </button>
+      <div className="task_container">
+        <li key={props.index}>
+          {props.name} {props.time}
+        </li>
+        <input
+          id={props.index}
+          onChange={(e) => setActualTaskName(e.target.value)}
+          value={actualTaskName}
+          placeholder="edit task here"
+        ></input>
+        <input
+          type="number"
+          id={props.index + 100}
+          onChange={(e) => setTime(e.target.value)}
+          value={time}
+          placeholder="edit time needed"
+        ></input>
+        <button
+          id="remove_btn"
+          onClick={() => props.handleRemoveTask(props.index)}
+        >
+          Remove
+        </button>
+        <button
+          id="edit_btn"
+          onClick={() =>
+            props.handleEditTask(props.index, actualTaskName, time)
+          }
+        >
+          Edit
+        </button>
+      </div>
     </>
   );
 };
