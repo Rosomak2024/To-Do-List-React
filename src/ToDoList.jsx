3653d4bf1de18d829bb69efdd38ac410faa878a1
Rosomak2024 <mbnet@o2.pl>
2024-03-13 12:11:38 +0100

    css added colors

--- src/ToDoList.jsx
+++ src/ToDoList.jsx
@@ -40,41 +40,45 @@ export const ToDoList = () => {
 
   return (
     <>
-      <input
-        id="inputName"
-        placeholder="add task name..."
-        value={task.name}
-        onChange={(e) => actualNameChange(e.target.value)}
-      ></input>
-      <input
-        type="number"
-        id="inputTime"
-        placeholder="add task time..."
-        value={task.time}
-        onChange={(e) => setTask({ ...task, time: e.target.value })}
-      ></input>
-      <button id="add_btn" onClick={handleAddTask}>
-        Add Task
-      </button>
-      <h2>
-        Czas potrzebny do wykonania {tasks.length} zadań to: {result} min;
-        <br></br> Time needed to finish {tasks.length} tasks is: {result} min;
-      </h2>
       <div id="container">
-        <ul>
-          {tasks.map((task, index) => {
-            return (
-              <ToDoListItem
-                key={index}
-                name={task.name}
-                index={index}
-                handleRemoveTask={handleRemoveTask}
-                handleEditTask={handleEditTask}
-                time={task.time}
-              />
-            );
-          })}
-        </ul>
+        <h3>THE TIME TASK APP</h3>
+        <div id="header_container">
+          <input
+            id="inputName"
+            placeholder="add task name..."
+            value={task.name}
+            onChange={(e) => actualNameChange(e.target.value)}
+          ></input>
+          <input
+            type="number"
+            id="inputTime"
+            placeholder="add task time..."
+            value={task.time}
+            onChange={(e) => setTask({ ...task, time: e.target.value })}
+          ></input>
+          <button id="add_btn" onClick={handleAddTask}>
+            Add Task
+          </button>
+        </div>
+        <h2>
+          Time needed to finish {tasks.length} tasks is: {result} min
+        </h2>
+        <div id="main_container">
+          <ul>
+            {tasks.map((task, index) => {
+              return (
+                <ToDoListItem
+                  key={index}
+                  name={task.name}
+                  index={index}
+                  handleRemoveTask={handleRemoveTask}
+                  handleEditTask={handleEditTask}
+                  time={task.time}
+                />
+              );
+            })}
+          </ul>
+        </div>
       </div>
     </>
   );
